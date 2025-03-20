1c5a69db61245396ba91c0540d8524e767838a4a
Berken Sayilir <sayilir.berken@gmail.com>
2025-03-20 11:32:26 +0100

    new test

--- Tests/MistTests/WebSocketConnection.swift
+++ Tests/MistTests/WebSocketConnection.swift
@@ -17,12 +17,7 @@ final class WebSocketConnection: XCTestCase
     func testSubscriptionDecoding() async
     {
         // create test json message
-        let text = """
-        {
-            "type": "subscribe",
-            "component": "TestComponent2"
-        }
-        """
+        let text = #"{ "type": "subscribe", "component": "TestComponent2" }"#
         
         // try to decode json message to mist message
         guard let data = text.data(using: .utf8) else { return XCTFail("Failed to convert JSON string to data") }
@@ -30,12 +25,12 @@ final class WebSocketConnection: XCTestCase
         
         switch message
         {
-                // ensure correct decoding
+            // ensure correct decoding
             case .subscribe(let component): do
-                {
-                    // test correct decoding of component name
-                    XCTAssertEqual(component, "TestComponent2", "Mist message component should match JSON component string")
-                }
+            {
+                // test correct decoding of component name
+                XCTAssertEqual(component, "TestComponent2", "Mist message component should match JSON component string")
+            }
                 
                 // ensure correct decoding
             default: return XCTFail("Valid but non-subscribe message")
@@ -53,7 +48,7 @@ final class WebSocketConnection: XCTestCase
         await Mist.registerComponents(using: config)
         
         // test this client message
-        let message = #"{ "type": "subscribe", "component": "DumbComp4133" }"#
+        let subscriptionMessage = #"{ "type": "subscribe", "component": "DumbComp4133" }"#
         
         // set up websocket on server
         app.webSocket("socket")
@@ -78,7 +73,7 @@ final class WebSocketConnection: XCTestCase
                 print("*** server receiving message: \(text)")
                 
                 // make sure sent client message and received server message match
-                XCTAssertEqual(text, message, "Sent and received message should match")
+                XCTAssertEqual(text, subscriptionMessage, "Sent and received message should match")
                 
                 // try to decode json message to typed mist message
                 guard let data = text.data(using: .utf8) else { return XCTFail("Failed to convert JSON string to data") }
@@ -103,7 +98,6 @@ final class WebSocketConnection: XCTestCase
                         XCTAssertEqual(connections.count, 1, "Only one client should exist")
                         XCTAssertEqual(connections[0].subscriptions.count, 1, "Client should have exactly one subscription")
                         XCTAssert(connections[0].subscriptions.contains("DumbComp4133"), "Client should be subscribed to component")
-                
                     }
                         
                     // ensure correct decoding
@@ -120,10 +114,134 @@ final class WebSocketConnection: XCTestCase
         { ws in
             Task
             {
-                print("*** client sending subscription message: \(message)")
+                print("*** client sending subscription message: \(subscriptionMessage)")
                 
                 // send component subscription message to client -> server
-                ws.send(message)
+                ws.send(subscriptionMessage)
+            }
+        }
+        
+        try await app.asyncShutdown()
+    }
+    
+    // tests integrated subscription message flow: client -> server -> internal storage registry
+    // tests integrated update message flow: server -> client
+    func testUpdateFlow() async throws
+    {
+        let app = try await Application.make(.testing)
+        app.databases.use(.sqlite(.memory), as: .sqlite)
+        
+        // register multiple components with dublicate
+        let config = Mist.Configuration(app: app, components: [DumbComp4133.self])
+        await Mist.registerComponents(using: config)
+        
+        let modelID = "F05F5571-A4D4-4227-B3D2-96B65BA9824A"
+        
+        let subscriptionMessage = #"{ "type": "subscribe", "component": "DumbComp4133" }"#
+        
+        let updateMessage =
+        """
+        {
+            "type": "componentUpdate",
+            "component": "DumbComp4133",
+            "action": "update",
+            "id": "\(modelID)",
+            "html": "<div mist-component=\\"DumbComp4133\\" mist-id=\\"\(modelID)\\">New Content</div>"
+        }
+        """
+                
+        // set up websocket on server
+        app.webSocket("socket")
+        { request, ws async in
+            
+            // create client
+            let clientID = UUID()
+            
+            // use API to add client to internal storage
+            await Mist.Clients.shared.add(connection: clientID, socket: ws)
+            
+            // get internal storage
+            let connections = await Mist.Clients.shared.connections
+            
+            // test internal storage after adding client
+            XCTAssertEqual(connections.count, 1, "Only one client should exist")
+            XCTAssertEqual(connections[0].id, clientID, "Client ID should match")
+            XCTAssertEqual(connections[0].subscriptions.count, 0, "Client should not have subscriptions")
+            
+            ws.onText()
+            { ws, text async in
+                print("*** server receiving message: \(text)")
+                
+                // make sure sent client message and received server message match
+                XCTAssertEqual(text, subscriptionMessage, "Sent and received message should match")
+                
+                // try to decode json message to typed mist message
+                guard let data = text.data(using: .utf8) else { return XCTFail("Failed to convert JSON string to data") }
+                guard let message = try? JSONDecoder().decode(Mist.Message.self, from: data) else { return XCTFail("Failed to decode data to Mist message") }
+                
+                switch message
+                {
+                    // ensure correct decoding
+                    case .subscribe(let component): do
+                    {
+                        // test correct decoding of component name
+                        XCTAssertEqual(component, "DumbComp4133", "Mist message component should match JSON component string")
+                        
+                        // use API to add client sent component name to client's subscription set
+                        let added = await Mist.Clients.shared.addSubscription(component, to: clientID)
+                        XCTAssertEqual(added, true, "Component not found (or client)")
+                        
+                        // get internal storage
+                        let connections = await Mist.Clients.shared.connections
+                        
+                        // test internal storage after adding subscription
+                        XCTAssertEqual(connections.count, 1, "Only one client should exist")
+                        XCTAssertEqual(connections[0].subscriptions.count, 1, "Client should have exactly one subscription")
+                        XCTAssert(connections[0].subscriptions.contains("DumbComp4133"), "Client should be subscribed to component")
+                        
+                        // after subscription, test component update
+                        print("*** server sending message: \(updateMessage)")
+                        try? await ws.send(updateMessage)
+                    }
+                        
+                    // ensure correct decoding
+                    default: return XCTFail("Valid but non-subscribe message")
+                }
+            }
+        }
+        
+        // start server (will block indefinitly and cause timeout, that's fine atm)
+        try await app.startup()
+        
+        // client connects to server socket
+        try await WebSocket.connect(to: "ws://localhost:8080/socket")
+        { ws in
+            Task
+            {
+                print("*** client sending subscription message: \(subscriptionMessage)")
+                
+                // send component subscription message to client -> server
+                ws.send(subscriptionMessage)
+            }
+            
+            ws.onText()
+            { ws, text in
+                print("*** client receiving message: \(text)")
+            
+                // try to decode json message to typed mist message
+                guard let data = text.data(using: .utf8) else { return XCTFail("Failed to convert JSON string to data") }
+                guard let message = try? JSONDecoder().decode(Mist.Message.self, from: data) else { return XCTFail("Failed to decode data to Mist message") }
+                
+                switch message
+                {
+                    case .componentUpdate(let component, _, let id, _): do
+                    {
+                        XCTAssertEqual(component, "DumbComp4133", "Update message for wrong component.")
+                        XCTAssertEqual(id?.uuidString, modelID, "Update message for wrong component.")
+                    }
+                        
+                    default: return XCTFail("Valid but non-update message")
+                }
             }
         }
         
